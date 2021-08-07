f87d9746c7c8139d24974ee99e71e719e647770f
manavtamboli <manavtamboli8@gmail.com>
2021-08-07 18:18:13 +0530

    Project Init

---
+++ app/src/main/java/com/codery/atheneum/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.codery.atheneum
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
