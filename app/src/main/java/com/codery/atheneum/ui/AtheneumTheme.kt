4ddde6f85a05bb8523b29e03b86c69867df53716
manavtamboli <manavtamboli8@gmail.com>
2021-08-07 18:36:40 +0530

    Dependencies Added

---
+++ app/src/main/java/com/codery/atheneum/ui/AtheneumTheme.kt
@@ -0,0 +1,6 @@
+package com.codery.atheneum.ui
+
+// Theme for Jetpack Compose
+object AtheneumTheme {
+
+}
--- app/src/main/java/com/codery/atheneum/ui/main/MainActivity.kt
+++ app/src/main/java/com/codery/atheneum/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.codery.atheneum
+package com.codery.atheneum.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.codery.atheneum.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
