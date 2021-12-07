42e5617beb4200f003d01d5ce8fc8bb22eb03f75
manavtamboli <manavtamboli8@gmail.com>
2021-12-07 14:44:17 +0530

    Login Fragment improved.

--- app/src/main/java/com/codery/atheneum/ui/login/LogInFragment.kt
+++ app/src/main/java/com/codery/atheneum/ui/login/LogInFragment.kt
@@ -3,8 +3,8 @@ package com.codery.atheneum.ui.login
 import android.content.Intent
 import android.util.Log
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import androidx.lifecycle.Lifecycle.State.STARTED
 import androidx.navigation.fragment.findNavController
 import com.codery.atheneum.R
 import com.codery.atheneum.databinding.FragmentLogInBinding
@@ -19,6 +19,9 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.manavtamboli.axion.binding.BindingFragment
 import com.manavtamboli.firefly.auth.GoogleSignInContract
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 class LogInFragment : BindingFragment<FragmentLogInBinding>(FragmentLogInBinding::class.java) {
@@ -41,16 +44,47 @@ class LogInFragment : BindingFragment<FragmentLogInBinding>(FragmentLogInBinding
             viewModel.firebaseAuthWithGoogle(acc.idToken ?: throw IllegalArgumentException("Id token cannot be null, but was null."))
         }
         .onFailure {
-                Log.d("error","onfailure $it")
+            Log.d("error","onfailure $it")
         }
     }
 
+    init {
+        lifecycleScope.launch {
+            repeatOnLifecycle(STARTED){
+                viewModel.state.collect {
+                    when(it) {
+                        is LoginState.Failed -> {
+                            // display failed message
+                            it.message
+                        }
+                        LoginState.Idle -> {
+                            // do nothing
+                        }
+                        LoginState.Loading -> {
+                            // make progress bar visible
+                        }
+                        LoginState.Registered -> {
+                            // navigate to main activity
+                            val  intent=Intent(requireContext(),MainActivity::class.java)
+                            startActivity(intent)
+                            requireActivity().finish()
+                        }
+                        LoginState.Unregistered -> {
+                            // navigate to register fragment
+                            findNavController().navigate(R.id.action_logInFragment_to_registerFragment)
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         if (currentUser != null){
+            viewModel.fetchUser()
             // user is signed in
 //            findNavController().navigate(R.id.action_logInFragment_to_dashboardFragment)
         }
@@ -72,21 +106,20 @@ class LogInFragment : BindingFragment<FragmentLogInBinding>(FragmentLogInBinding
         btnSignOut.setOnClickListener {
             signOut()
         }
-        viewModel.isRegister.observe(viewLifecycleOwner){ isRegistered ->
-            isRegistered ?: return@observe
-            if (isRegistered){
-                Log.d("tagged","isregisterd true")
-                val  intent=Intent(requireContext(),MainActivity::class.java)
-                startActivity(intent)
-                requireActivity().finish()
-            }
-
-            else{
-//                Log.d("tagged","false isregistered")
-                findNavController().navigate(R.id.action_logInFragment_to_registerFragment)
-            }
-
-        }
+//        viewModel.isRegister.observe(viewLifecycleOwner){ isRegistered ->
+//            isRegistered ?: return@observe
+//            if (isRegistered){
+//                Log.d("tagged","isregisterd true")
+//                val  intent=Intent(requireContext(),MainActivity::class.java)
+//                startActivity(intent)
+//                requireActivity().finish()
+//            }
+//
+//            else{
+////                Log.d("tagged","false isregistered")
+//                findNavController().navigate(R.id.action_logInFragment_to_registerFragment)
+//            }
+//        }
 
 
     }
@@ -98,35 +131,42 @@ class LoginViewModel : ViewModel(){
 
     val isRegister : MutableLiveData<Boolean> = MutableLiveData()
     private val auth = Firebase.auth
-    lateinit var emailList:MutableLiveData<Email>
 
-    // Idle
+//    val state : MutableLiveData<LoginState> = MutableLiveData()
+
+    val state : MutableStateFlow<LoginState> = MutableStateFlow(LoginState.Idle)
+
     fun firebaseAuthWithGoogle(idToken: String) {
         // Loading
+        state.value = LoginState.Loading
+
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential)
             .addOnSuccessListener {
-                val checkemail = auth.currentUser?.email
-                val collectionReference = Firebase.firestore.collection("DGV")
-                    .whereEqualTo("Email", auth.currentUser?.email)
-                    .get()
-                    .addOnSuccessListener {
-                        if (it.documents.isEmpty()) {
-                            // UnRegistered User
-                            isRegister.value=false
-
-                        } else {
-                            // Registered User
-                            isRegister.value = true
-                        }
-
-                    }
-                    .addOnFailureListener {
-                        // Failed
-                    }
+                fetchUser()
             }
             .addOnFailureListener {
-                // Failed
+                state.value = LoginState.Failed("asdfas")
             }
     }
+
+    fun fetchUser(){
+        Firebase.firestore.collection("DGV")
+            .whereEqualTo("Email", auth.currentUser?.email)
+            .get()
+            .addOnSuccessListener {
+                state.value = if (it.documents.isNotEmpty()) LoginState.Registered else LoginState.Unregistered
+            }
+            .addOnFailureListener {
+                state.value = LoginState.Failed("asdbfiausdbf")
+            }
+    }
+}
+
+sealed class LoginState {
+    object Idle : LoginState()
+    object Loading : LoginState()
+    object Registered : LoginState()
+    object Unregistered : LoginState()
+    data class Failed(val message : String) : LoginState()
 }
